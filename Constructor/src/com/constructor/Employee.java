8aed26ae29aa2e989c6fdf3519e39f5dd8f92c5f
aditya <aditya@nameisadi>
2025-01-27 01:42:09 +0530

    Constructor pro

---
+++ Constructor/src/com/constructor/Employee.java
@@ -0,0 +1,19 @@
+package com.constructor;
+
+public class Employee {
+
+	
+	int id;
+	String name;
+	double salary;
+	
+	 Employee()
+		{
+			this.id=id;
+			this.name=name;
+			this.salary=salary;
+		}
+		
+	
+	
+}
---
+++ Constructor/src/com/constructor/Test.java
@@ -0,0 +1,20 @@
+package com.constructor;
+
+public class Test {
+	
+	public static void main(String args[]) {
+		
+		Employee e1=new Employee();
+		e1.id=1;
+		e1.name="aaa";
+		e1.salary=40000d;
+	
+		System.out.println(e1.id);
+		System.out.println(e1.name);
+		System.out.println(e1.salary);
+		
+		
+		
+	}
+
+}
